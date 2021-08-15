a2f3797279294ecf65ef8b4147a21d669bd051a0
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-08-14 21:42:28 -0300

    Done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,24 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import logoImg from '../assets/images/logo/logo.png';
 
-interface HeaderProps {
+type HeaderProps = {
   tasksCounter: number;
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
-  // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
-  
+  //DONE render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+  const tasksCounterText = tasksCounter === 1 ? 'tarefa' : 'tarefas';
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} />
       
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        
+        <Text style={styles.tasksCounterBold}>
+          {tasksCounter} {tasksCounterText}
+        </Text>
       </View>
     </View>
   )
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet, FlatListProps } from 'react-native';
+import { 
+  FlatList, 
+  Image, 
+  TouchableOpacity, 
+  View, 
+  Text, 
+  StyleSheet, 
+  FlatListProps 
+} from 'react-native';
+
 import Icon from 'react-native-vector-icons/Feather';
 
 import { ItemWrapper } from './ItemWrapper';
@@ -21,7 +30,7 @@ interface TasksListProps {
 export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
   return (
     <FlatList
-      // data={tasks}
+      data={tasks}
       keyExtractor={item => String(item.id)}
       contentContainerStyle={{ paddingBottom: 24 }}
       showsVerticalScrollIndicator={false}
@@ -33,23 +42,28 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
+                //DONE - use onPress (toggle task) prop
+                onPress={() => toggleTaskDone(item.id)}
               >
                 <View 
                   testID={`marker-${index}`}
-                  //TODO - use style prop 
+                  //DONE - use style prop
+                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
                 >
-                  { item.done && (
-                    <Icon 
-                      name="check"
-                      size={12}
-                      color="#FFF"
-                    />
-                  )}
+                  { 
+                    item.done && (
+                      <Icon 
+                        name="check"
+                        size={12}
+                        color="#FFF"
+                      />
+                    )
+                  }
                 </View>
 
                 <Text 
-                  //TODO - use style prop
+                  //DONE - use style prop
+                  style={item.done ? styles.taskTextDone : styles.taskText}
                 >
                   {item.title}
                 </Text>
@@ -59,7 +73,8 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
             <TouchableOpacity
               testID={`trash-${index}`}
               style={{ paddingHorizontal: 24 }}
-              //TODO - use onPress (remove task) prop
+              //DONE - use onPress (remove task) prop
+              onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
             </TouchableOpacity>
